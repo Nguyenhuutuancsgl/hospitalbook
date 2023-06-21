d2762ae082b952b370c1cfb15c3ac5fad215fc86
TuanTA <nguyenhuutuancsgl@gmail.com>
2023-06-21 16:24:30 +0700

    thulamdep

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,9 +5,9 @@ function route(app) {
     // app.get('/news', (req, res) => {
     //     res.render('news');
     // });
-    app.use('/news', newsRouter);
+          app.use('/news', newsRouter);
 
-    app.use('/', siteRouter);
+        app.use('/', siteRouter);
 }
 
 module.exports = route;
